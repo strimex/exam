4a88868aa835355c00e62be0ca7472c4d511cdb4
strimex <70368430+strimex@users.noreply.github.com>
2021-07-07 16:46:49 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,1 +1,10 @@
+import time
 
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_verify_add_to_basket_button(browser):
+    browser.get(link)
+    time.sleep(10)
+    add_to_basket_button = browser.find_element_by_xpath("//button[contains(text(),'Добавить в корзину')]")
+    assert add_to_basket_button, "There is no Add to Basket button!"
