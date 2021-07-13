2939340854efdec7aafd18458cee88bab11453b1
strimex <deleteacc11@gmail.com>
2021-07-13 17:08:40 +0300

    last changes

--- conftest.py
+++ conftest.py
@@ -2,21 +2,30 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 def pytest_addoption(parser):
-    parser.addoption("--language", action="store", default="en",
-					 help="Choose language")
+    parser.addoption('--browser_name', action='store', default="chrome",
+                                 help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="ru",
+                                 help="Choose language: ru, en ...")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        options = Options()
+        options.add_experimental_option("excludeSwitches", ["enable-logging"])
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
--- test_items.py
+++ test_items.py
@@ -3,8 +3,8 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_verify_add_to_basket_button(browser):
+def test_button_add_to_basket_exist_on_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(10)
-    add_to_basket_button = browser.find_element_by_xpath("//button[contains(text(),'Добавить в корзину')]")
-    assert add_to_basket_button, "There is no Add to Basket button!"
+    time.sleep(5)
+    assert browser.find_element_by_css_selector('.btn.btn-lg.btn-primary.btn-add-to-basket')
